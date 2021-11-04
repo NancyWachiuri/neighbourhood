8d0d93dd1bda7c8daac8cf95a45c4267af0a9c7f
NancyWachiuri <nancywachiuri@gmail.com>
2021-11-04 08:42:52 +0300

    registered model classes in admin.py

--- neighbor/admin.py
+++ neighbor/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import NeighbourHood,Business,Profile
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(NeighbourHood)
+admin.site.register(Business)
