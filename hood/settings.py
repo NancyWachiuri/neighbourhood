db9f8fed26b2cc26a492c7877ecee7c41ca8ac3c
NancyWachiuri <nancywachiuri@gmail.com>
2021-11-05 08:14:44 +0300

    Deploy

--- hood/settings.py
+++ hood/settings.py
@@ -160,7 +160,7 @@ STATICFILES_DIRS = [
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.patcomh.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
